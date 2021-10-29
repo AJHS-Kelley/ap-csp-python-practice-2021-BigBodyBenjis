14987339e1af10b408afb855964773a45e6ce04b
BigBodyBenjis <90343481+BigBodyBenjis@users.noreply.github.com>
2021-10-29 13:02:51 -0400

    v0.1
    Added 3 variables: integers, a floating point, and a string.

---
+++ .github/variables/variables_practice.py
@@ -0,0 +1,8 @@
+# Variables Practice, Terrell Blathers, 10/29/2021 1:01pm, version 0.1
+
+
+# Basic Variables
+
+my_int = 420 # INTEGERS, any +/- whole number and 0
+my_float = 2.67 # FLOATING POINT, any +/- number with a decimal.
+my_string = "The Los Angeles Lakers is the NBA equivalent of a retirement home"
