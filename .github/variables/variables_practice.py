81b16e87520efe2494c093cb69240b2849761707
BigBodyBenjis <90343481+BigBodyBenjis@users.noreply.github.com>
2021-10-29 13:07:55 -0400

    v0.2
    Added 3 print commands for my_int, my_float, and my_string.

--- .github/variables/variables_practice.py
+++ .github/variables/variables_practice.py
@@ -1,4 +1,4 @@
-# Variables Practice, Terrell Blathers, 10/29/2021 1:01pm, version 0.1
+# Variables Practice, Terrell Blathers, 10/29/2021 1:07pm, version 0.2
 
 
 # Basic Variables
@@ -6,3 +6,7 @@
 my_int = 420 # INTEGERS, any +/- whole number and 0
 my_float = 2.67 # FLOATING POINT, any +/- number with a decimal.
 my_string = "The Los Angeles Lakers is the NBA equivalent of a retirement home"
+
+print(my_int)
+print(my_float)
+print(my_string)
