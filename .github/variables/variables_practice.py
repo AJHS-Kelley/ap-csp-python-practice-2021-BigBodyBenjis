5d9e79089a7791f3cf6a3b3440fc52830bf0f938
BigBodyBenjis <90343481+BigBodyBenjis@users.noreply.github.com>
2021-10-29 13:22:06 -0400

    v0.4

--- .github/variables/variables_practice.py
+++ .github/variables/variables_practice.py
@@ -1,4 +1,4 @@
-# Variables Practice, Terrell Blathers, 10/29/2021 1:07pm, version 0.2
+# Variables Practice, Terrell Blathers, 10/29/2021 1:21pm, version 0.3
 
 
 # Basic Variables
@@ -10,3 +10,12 @@ my_string = "The Los Angeles Lakers is the NBA equivalent of a retirement home"
 print(my_int)
 print(my_float)
 print(my_string)
+
+my_int = input ("Write the number of siblings you have.\n")
+print(my_int)
+
+my_float = input ("What is your 100m dash time?\n")
+print(my_int)
+
+my_string = input ("The NBA 75th Anniversary Team\n")
+print(my_int)
